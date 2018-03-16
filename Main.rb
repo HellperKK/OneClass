19ea3084a36d8a74011bfed29661b651af0cfceb
François Berhn <baudetad@gmail.com>
2018-03-16 13:34:13 +0100

    Update

--- Main.rb
+++ Main.rb
@@ -5,17 +5,35 @@ class Game < Gosu::Window
 		self.caption = "Game"
 		@font = Gosu::Font.new(18)
 		@input = []
+    @scene = ""
+    go_to_scene("intro")
 		# @input = InputManager.new([Gosu::KbUp, Gosu::KbDown, Gosu::KbLeft, Gosu::KbRight, Gosu::KbSpace, Gosu::MsLeft], lambda{|i| button_down?(i)})
 	end
 	def update
 		if @input.include?(Gosu::KbEscape)
 			self.close!
 		end
+    method("scene_#{@scene}_update".to_s).call
 		@input = []
 	end
 	def draw
-    
+    method("scene_#{@scene}_draw".to_s).call
 	end
+  def go_to_scene(name)
+    @scene = name
+    method("scene_#{@scene}_init".to_s).call
+  end
+
+  #Intro scene
+  def scene_intro_init
+
+  end
+  def scene_intro_update
+
+  end
+  def scene_intro_draw
+
+  end
 
   def button_down(id)
 		@input << id
