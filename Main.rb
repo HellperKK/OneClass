87b21328ff6e02e46b7517bf8d21cb46c59743fd
François Berhn <baudetad@gmail.com>
2018-03-17 09:58:42 +0100

    Update

--- Main.rb
+++ Main.rb
@@ -6,6 +6,7 @@ class Game < Gosu::Window
 		@font = Gosu::Font.new(18)
 		@input = []
     @scene = ""
+    @elements = []
     go_to_scene("intro")
 		# @input = InputManager.new([Gosu::KbUp, Gosu::KbDown, Gosu::KbLeft, Gosu::KbRight, Gosu::KbSpace, Gosu::MsLeft], lambda{|i| button_down?(i)})
 	end
@@ -13,26 +14,48 @@ class Game < Gosu::Window
 		if @input.include?(Gosu::KbEscape)
 			self.close!
 		end
-    method("scene_#{@scene}_update".to_s).call
+    method("scene_#{@scene}_update".to_sym).call
 		@input = []
 	end
 	def draw
-    method("scene_#{@scene}_draw".to_s).call
+    method("scene_#{@scene}_draw".to_sym).call
 	end
   def go_to_scene(name)
     @scene = name
-    method("scene_#{@scene}_init".to_s).call
+    method("scene_#{@scene}_init".to_sym).call
   end
 
   #Intro scene
   def scene_intro_init
-
+    @elements = [
+      {
+        x: 320,
+        y: 160,
+        sprite: Gosu::Image.new("Sprites/Carre.png")
+      },
+      {
+        x: 320,
+        y: 320,
+        sprite: Gosu::Image.new("Sprites/Carre.png")
+      }
+    ]
   end
   def scene_intro_update
-
+    if button_down?(Gosu::KbUp)
+      @elements[0][:y] -= 1
+    end
+    if button_down?(Gosu::KbDown)
+      @elements[0][:y] += 1
+    end
+    if button_down?(Gosu::KbLeft)
+      @elements[0][:x] -= 1
+    end
+    if button_down?(Gosu::KbRight)
+      @elements[0][:x] += 1
+    end
   end
   def scene_intro_draw
-
+    @elements.each{|i| i[:sprite].draw(i[:x], i[:y], 0)}
   end
 
   def button_down(id)
@@ -40,9 +63,6 @@ class Game < Gosu::Window
 	end
   def pressed?(id)
     @input.include?(id)
-  end
-  def pressing?(id)
-    button_down?(id)
   end
 end
 
